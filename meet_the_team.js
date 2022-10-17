5aef59e6df2f4fe3c43da6c8c02c5414bb98cd43
Auriel-Wish <44419323+Auriel-Wish@users.noreply.github.com>
2022-10-17 12:25:36 -0400

    Update meet_the_team.js

--- meet_the_team.js
+++ meet_the_team.js
@@ -9,7 +9,7 @@ function create_elements() {
     for (i = 0; i < 2; i++) {
         total_add += '<tr>';
         for (j = (2 * i); j < (2 * (i + 1)); j++) {
-            total_add += '<td><div><img src="images/member' + (j + 1) + '.jpg"><a href="#p' + (j + 1) + '"><i id="p' + (j + 1) + '" class="fa-solid fa-circle-chevron-down"></i></a><h2>' + all_names[j] + '</h2><p>' + all_descriptions[j] + '</p></div></td>';
+            total_add += '<td><div><h2>' + all_names[j] + '</h2><img src="images/member' + (j + 1) + '.jpg"><a href="#p' + (j + 1) + '"><i id="p' + (j + 1) + '" class="fa-solid fa-circle-chevron-down"></i></a><p>' + all_descriptions[j] + '</p></div></td>';
         }
         total_add += '</tr>';
     }
